0ed5b9f463fa51c6093d9cdf10b75e0c057eaf51
Niraj_Gohel <37961248+niraj281997@users.noreply.github.com>
2025-09-13 14:47:46 +0530

    Create Test.py

---
+++ cuda/ORB-SLAM/Test.py
@@ -0,0 +1,5 @@
+import torch
+
+print(torch.cuda.is_available())  # Returns True if GPU is available
+print(torch.cuda.device_count())  # Number of GPUs
+print(torch.cuda.get_device_name(0))  # Name of the first GPU
